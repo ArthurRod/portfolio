ccef635a2c3318732515e9c30338de5d8c7360e1
Arthur Rodrigues <arthur9713@hotmail.com>
2023-05-29 16:36:41 -0300

    Revisão Layout finalizada

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -12,43 +12,45 @@ export function Resume({ resume }: ResumeProps) {
   return (
     <section id="resume" className="resume" aria-label="Seção Resumo">
       <div className="container">
-        <div className="image" aria-label="Imagem">
-          <img
-            src={image}
-            alt={`Imagem de ${name}`}
-            width={225}
-            loading="lazy"
-          />
-        </div>
-        <div className="infos" aria-label="Informações principais">
-          <div className="name-position" aria-label="Nome e cargo">
-            <div className="name" aria-label="Nome">
-              <h2>Hello world!</h2>
-              <h1>I'm {name}</h1>
-            </div>
-            <div className="position" aria-label="Cargo">
-              <h2>{position}</h2>
-            </div>
+        <div className="image-infos">
+          <div className="image" aria-label="Imagem">
+            <img
+              src={image}
+              alt={`Imagem de ${name}`}
+              width={225}
+              loading="lazy"
+            />
           </div>
+          <div className="infos" aria-label="Informações principais">
+            <div className="name-position" aria-label="Nome e cargo">
+              <div className="name" aria-label="Nome">
+                <h2>Hello world!</h2>
+                <h1>I'm {name}</h1>
+              </div>
+              <div className="position" aria-label="Cargo">
+                <h2>{position}</h2>
+              </div>
+            </div>
 
-          <div className="resume-description" aria-label="Descrição curta">
-            <p>
-              Engenheiro de Software formado pela Unicesumar e desenvolvedor
-              Front-End Web. Há mais de dois anos atuando com implantação e
-              manutenção de e-commerce VTEX IO e VTEX CMS utilizando as
-              tecnologias React, TypeScript, JavaScript, HTML5, CSS3 e Sass pela
-              agência&nbsp;
-              <a target="_blank" href="https://www.siriusdigital.com.br/">
-                Sirius Digital,
-              </a>
-              &nbsp;uma empresa subsidiária da&nbsp;
-              <a target="_blank" href="https://leste.totvs.com/">
-                TOTVS Leste.
-              </a>
-            </p>
+            <div className="resume-description" aria-label="Descrição curta">
+              <p>
+                Engenheiro de Software formado pela Unicesumar e desenvolvedor
+                Front-End Web. Há mais de dois anos atuando com implantação e
+                manutenção de e-commerce VTEX IO e VTEX CMS utilizando as
+                tecnologias React, TypeScript, JavaScript, HTML5, CSS3 e Sass
+                pela agência&nbsp;
+                <a target="_blank" href="https://www.siriusdigital.com.br/">
+                  Sirius Digital,
+                </a>
+                &nbsp;uma empresa subsidiária da&nbsp;
+                <a target="_blank" href="https://leste.totvs.com/">
+                  TOTVS Leste.
+                </a>
+              </p>
+            </div>
+
+            <SocialMedias socialMedias={socialMedias} />
           </div>
-
-          <SocialMedias socialMedias={socialMedias} />
         </div>
       </div>
     </section>
